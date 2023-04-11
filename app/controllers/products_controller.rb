5075629a14de2c6d745d74fc7fe34ec3113a3db4
Marcelo Manoel <marcelomanoelsilvaf@gmail.com>
2023-04-10 21:28:54 -0300

    CRUD concluid

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,43 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to product_path(@product)
+  end
+
+  def edit
+  end
+
+  def update
+    @product.update(product_params)
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to product_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :photo)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
