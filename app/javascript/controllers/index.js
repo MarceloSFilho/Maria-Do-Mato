2be5707e1126b355fd12a2f4a0bc041d500d2060
Marcelo Manoel <marcelomanoelsilvaf@gmail.com>
2023-04-12 18:45:18 -0300

    style for page index concluid

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import OnHoverController from "./on_hover_controller"
+application.register("on-hover", OnHoverController)
