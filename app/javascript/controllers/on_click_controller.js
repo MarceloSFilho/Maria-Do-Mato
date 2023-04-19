6a286fb237ca8ad438d84712d598110c19426014
Marcelo Manoel <marcelomanoelsilvaf@gmail.com>
2023-04-19 17:35:17 -0300

    modal concluid

--- app/javascript/controllers/on_click_controller.js
+++ app/javascript/controllers/on_click_controller.js
@@ -2,7 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="on-click"
 export default class extends Controller {
-  static targets=['edit', 'input', 'update']
+  static targets=['modal', 'overlay']
   connect() {
+  }
+
+  addModal() {
+    this.modalTarget.classList.remove("d-none")
+  }
+
+  removeModal() {
+    this.modalTarget.classList.add("d-none")
+  }
+
+  addOverlay() {
+    this.overlayTarget.classList.remove("display-none")
+  }
+
+  removeOverlay() {
+    this.overlayTarget.classList.add("display-none")
   }
 }
