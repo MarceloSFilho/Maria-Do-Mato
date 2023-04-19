7ae654615ba58cf62d29d5e31a44e6a8d4954be5
Marcelo Manoel <marcelomanoelsilvaf@gmail.com>
2023-04-19 12:06:11 -0300

    add style in several pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_many :filter, dependent: :destroy
   has_one_attached :photo
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 end
