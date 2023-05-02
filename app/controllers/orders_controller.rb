49f1ec498812cd91a27a51ac82e1009018bfa7d0
Marcelo Manoel <marcelomanoelsilvaf@gmail.com>
2023-05-02 13:34:17 -0300

    add in herokuapp

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,8 +18,7 @@ class OrdersController < ApplicationController
         quantity: 1,
       }],
       mode: 'payment',
-      success_url: order_url(order),
-      cancel_url: order_url(order)
+      success_url: "https://mariadomato.herokuapp.com/orders/#{order}/payments/new"
     )
 
     order.update(checkout_session_id: session.id)
