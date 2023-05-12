162247f346b82bcadc09f7462762a3afba77a8ef
marcelo manoel <marcelomanoelsilvaf@gmail.com>
2023-05-12 15:19:48 -0300

    pg search concluid

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,15 @@ class Product < ApplicationRecord
   has_many :filter, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_one_attached :photo
-
-
   monetize :price_cents
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_products,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
