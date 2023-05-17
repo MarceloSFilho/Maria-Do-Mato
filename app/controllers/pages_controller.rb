dd054afd9f49400266477b81c760be4a332f969d
marcelo manoel <marcelomanoelsilvaf@gmail.com>
2023-05-17 17:21:18 -0300

    fixed footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :manufacturing]
 
   def home
+    @products = policy_scope(Product).limit(4)
   end
 end
