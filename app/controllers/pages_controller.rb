ce30b36664f55542612363ae22cc26d5539535ca
marcelo manoel <marcelomanoelsilvaf@gmail.com>
2023-05-21 01:06:35 -0300

    fixed error devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :manufacturing]
+  skip_before_action :authenticate_user!, only: :home
 
   def home
     @products = policy_scope(Product).limit(4)
